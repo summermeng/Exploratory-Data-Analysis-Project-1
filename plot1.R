f03eb9a8572463a7100d818e4043e815fdad504f
summermeng <mengxia0912@gmail.com>
2014-05-11 14:48:36 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,4 +4,5 @@ attach(hpc)
 mydata <- hpc[66638:69517, ]
 dim(mydata)
 Global_active_power <- as.numeric(mydata$V3)
-hist(Global_active_power,col="red", main="Global Active Power", xlab= "Global Active Power(kilowatt)")
+plot1 <- hist(Global_active_power,col="red", main="Global Active Power", xlab= "Global Active Power(kilowatt)")
+dev.print(png, file = "plot1.png", width = 480, height = 480)
